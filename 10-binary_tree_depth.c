3aceb6438e041eabcb3a449cfb8cd01f827d0ff0
KARAM022 <my0fav0library@gmail.com>
2024-03-26 00:44:41 +0000

    binary_trees

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,5 +9,8 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
+	if (tree && tree->parent)
+		return  (1 + binary_tree_depth(tree->parent));
+	else
+		return (0);
 }
